1e657a8425754ff99c66ef52f03f3550c177ca49
unknown <l4wl3ss1989@gmail.com>
2019-12-01 21:59:25 +0100

    swap

--- src/shared/components/UIElements/Navigation/MainNavigation/MainNavigation.js
+++ src/shared/components/UIElements/Navigation/MainNavigation/MainNavigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from '../MainHeader/MainHeader';
+import NavLinks from '../NavLinks/NavLinks';
 import './MainNavigation.scss';
 
 const MainNavigation = () => (
@@ -14,7 +15,9 @@ const MainNavigation = () => (
     <h1 className="main-navigation__title">
       <Link to="/">Your Places</Link>
     </h1>
-    <nav>...</nav>
+    <nav>
+      <NavLinks />
+    </nav>
   </MainHeader>
 );
 
